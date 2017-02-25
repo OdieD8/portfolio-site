091ee9bc89bf85b2e82bc0c674f79cc7f4da7e4a
OdieD8 <odie_de_moura@hotmail.com>
2017-02-25 15:52:43 -0700

    Portfolio page mvp complete

--- client/routes.js
+++ client/routes.js
@@ -13,5 +13,23 @@ angular.module("app", ["ui.router"]).config(function($urlRouterProvider, $stateP
 			controller: "portfolioController"	
 		})
 	
+		.state("bowleague", {
+			url: "/bowleague",
+			templateUrl: "features/portfolio/bowleague.template.html",
+			controller: "portfolioController"
+		})
+	
+		.state("clout", {
+			url: "/clout",
+			templateUrl: "features/portfolio/clout.template.html",
+			controller: "portfolioController"
+		})
+	
+		.state("storelist", {
+			url: "/storelist",
+			templateUrl: "features/portfolio/storelist.template.html",
+			controller: "portfolioController"
+		})
+	
     $urlRouterProvider.otherwise("/home");
 });
