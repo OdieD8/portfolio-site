e8789913cf174b95748d18c1378d6a2634e02054
OdieD8 <odie_de_moura@hotmail.com>
2019-02-18 18:40:38 -0700

    firebase rewrite config

--- client/routes.js
+++ client/routes.js
@@ -2,45 +2,43 @@ angular.module("app", ["ui.router"]).config(function($urlRouterProvider, $stateP
 
     $stateProvider
         .state("home", {
-            url: "/home",
+            url: "#/home",
             templateUrl: "features/home/home.template.html",
             controller: "homeController"
         })
 
 		.state("portfolio", {
-			url: "/portfolio",
+			url: "#/portfolio",
 			templateUrl: "features/portfolio/portfolio.template.html",
 			controller: "portfolioController"
 		})
 
 		.state("bowleague", {
-			url: "/bowleague",
+			url: "#/bowleague",
 			templateUrl: "features/portfolio/bowleague.template.html",
 			controller: "portfolioController"
 		})
 
 		.state("clout", {
-			url: "/clout",
+			url: "#/clout",
 			templateUrl: "features/portfolio/clout.template.html",
 			controller: "portfolioController"
 		})
 
 		.state("storelist", {
-			url: "/storelist",
+			url: "#/storelist",
 			templateUrl: "features/portfolio/storelist.template.html",
 			controller: "portfolioController"
 		})
 
 		.state("about", {
-			url: "/about",
+			url: "#/about",
 			templateUrl: "features/about/about.template.html"
 		})
 
 		.state("contact", {
-			url: "/contact",
+			url: "#/contact",
 			templateUrl: "features/contact/contact.template.html"
 		})
-
-// 		$locationProvider.html5Mode(true);
-		$urlRouterProvider.otherwise("/home");
+		$urlRouterProvider.otherwise("#/home");
 });
